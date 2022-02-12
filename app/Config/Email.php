b1ae56f92e74e80bc523aaafb9eb6566cf93196d
TsaqifArkan <scalled040201@gmail.com>
2022-02-12 11:57:54 +0700

    konfigurasi awal halaman login dan register

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -9,12 +9,12 @@ class Email extends BaseConfig
     /**
      * @var string
      */
-    public $fromEmail;
+    public $fromEmail = 'scalledcoc@gmail.com';
 
     /**
      * @var string
      */
-    public $fromName;
+    public $fromName = 'Aveneraa Scalled';
 
     /**
      * @var string
