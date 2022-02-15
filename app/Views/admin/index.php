59c89c335bb08c99d6af5aadbc33d148cbc42f2a
TsaqifArkan <scalled040201@gmail.com>
2022-02-15 23:47:14 +0700

    early configure user list view

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -11,31 +11,40 @@
 
     <div class="row">
         <div class="col-lg-8">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Username</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Role</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php $i = 1; ?>
-                    <?php foreach ($users as $user) : ?>
-                        <tr>
-                            <th scope="row"><?= $i++; ?></th>
-                            <td><?= $user->username; ?></td>
-                            <td><?= $user->email; ?></td>
-                            <td><?= $user->name; ?></td>
-                            <td>
-                                <a href="<?= base_url('admin/' . $user->userid); ?>" class="btn btn-info">detail</a>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Tabel Daftar User</h6>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered" id="dataTable">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Username</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Role</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $i = 1; ?>
+                                <?php foreach ($users as $user) : ?>
+                                    <tr>
+                                        <th scope="row"><?= $i++; ?></th>
+                                        <td><?= $user->username; ?></td>
+                                        <td><?= $user->email; ?></td>
+                                        <td><?= $user->name; ?></td>
+                                        <td class="d-flex justify-content-center">
+                                            <a href="<?= base_url('admin/' . $user->userid); ?>" class="btn btn-info">detail</a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
