093a59169ec2d96df44a2fe6f8681c3c52892e06
TsaqifArkan <scalled040201@gmail.com>
2022-02-26 22:26:41 +0700

    commented script and initiate sweetalert

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,8 @@
 const currentLocation = location.href;
-console.log(currentLocation);
+// console.log(currentLocation);
 // const aActiveToggler = document.querySelectorAll('ul.parent-active-toggler li.active-toggler a');
 const aActiveToggler = document.querySelectorAll('.nav-link');
-console.log(aActiveToggler);
+// console.log(aActiveToggler);
 const aATLength = aActiveToggler.length;
 const aMyProfile = document.querySelector('.default-a-index');
 
@@ -22,16 +22,48 @@ for (let i = 0; i < aATLength; i++){
         aActiveToggler[i].parentElement.classList.add('active');
         
     }
-    console.log(aActiveToggler[i].parentElement);
+    // console.log(aActiveToggler[i].parentElement);
 }
 
-// JQuery Simplified Version -- but not for this app
+// JQuery Simplified Version for toggle active class in sidebar -- but not for this app
 // $(document).on('click', '.nav-item.active-toggler', function(){
 //     $(this).addClass('active').siblings().removeClass('active');
 // });
-
 
 // JQUERY - MODAL
 $('#formModal').on('shown.bs.modal', function() {
     $('#nama').focus();
 })
+
+// SweetAlert2 - JQuery
+// 1.Konfigurasi flash data
+const flashData = $('.flash-data').data('flashdata');
+// console.log(flashData);
+if(flashData){
+    Swal.fire({
+        title : 'SUCCESS !',
+        text : flashData,
+        icon : 'success'
+    });
+}
+// 2.Konfigurasi tombol hapus
+// $('.tombol-hapus').on('click', function(e){
+//     e.preventDefault();
+//     const href = $(this).data('id');
+//     console.log(href);
+
+//     Swal.fire({
+//         title: 'Are you sure ?',
+//         text: "You won't be able to undo this action!",
+//         icon: 'warning',
+//         showCancelButton: true,
+//         confirmButtonColor: '#3085d6',
+//         cancelButtonColor: '#d33',
+//         confirmButtonText: 'Yes, delete it!'
+//       }).then((result) => {
+//         if (result.isConfirmed) {
+//             // document.location.href = href;
+//             window.location.replace(href);
+//         }
+//       })
+// });
